d3e1f218436be9ca6063755b2dad5eefa95c9156
yangxiaochuan <xiaochuan.yang@vivo.com>
2019-05-21 19:31:40 +0800

    1

--- app/controller/home.js
+++ app/controller/home.js
@@ -6,11 +6,13 @@ class HomeController extends Controller {
   async index() {
     const {
       ctx,
-      app
+      app,
+      logger
     } = this
     console.log('当前运行环境：', app.config.env)
     console.log('当前config key：', app.config.keys)
     this.ctx.fun1()
+    // logger.warn('测试log')
     // this.ctx.body = 'Hello world';
     await ctx.render('index.njk');
   }
